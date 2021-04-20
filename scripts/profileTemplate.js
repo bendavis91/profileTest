bc33edc687a502a7b9db6dc4100a15cd57555a02
bendavis91 <60115174+bendavis91@users.noreply.github.com>
2021-04-20 15:40:45 -0500

    Update profileTemplate.js

--- scripts/profileTemplate.js
+++ scripts/profileTemplate.js
@@ -34,6 +34,76 @@ xmlhttp.onreadystatechange = function () {
         document.getElementById("email").innerHTML = myObj.devProfiles[1].email;
         document.getElementById("githubProfile").innerHTML = myObj.devProfiles[1].githubProfile;
         
+      }else if(window.location.pathname == '/gant-dev-profile.html'){
+       
+        document.getElementById("name").innerHTML = myObj.devProfiles[2].name;
+        document.getElementById("major").innerHTML = myObj.devProfiles[2].major;
+        document.getElementById("concentration").innerHTML = myObj.devProfiles[2].concentration;
+        document.getElementById("degree").innerHTML = myObj.devProfiles[2].degree;
+        document.getElementById("languages").innerHTML = myObj.devProfiles[2].skills.languages;
+        document.getElementById("frameworks").innerHTML = myObj.devProfiles[2].skills.frameworks;
+        document.getElementById("database").innerHTML = myObj.devProfiles[2].skills.database;
+        document.getElementById("position").innerHTML = myObj.devProfiles[2].skills.position;
+        document.getElementById("description").innerHTML = myObj.devProfiles[2].description;
+        document.getElementById("email").innerHTML = myObj.devProfiles[2].email;
+        document.getElementById("githubProfile").innerHTML = myObj.devProfiles[2].githubProfile;
+
+      }else if(window.location.pathname == '/Estacio-dev-profile.html'){
+       
+        document.getElementById("name").innerHTML = myObj.devProfiles[3].name;
+        document.getElementById("major").innerHTML = myObj.devProfiles[3].major;
+        document.getElementById("concentration").innerHTML = myObj.devProfiles[3].concentration;
+        document.getElementById("degree").innerHTML = myObj.devProfiles[3].degree;
+        document.getElementById("languages").innerHTML = myObj.devProfiles[3].skills.languages;
+        document.getElementById("frameworks").innerHTML = myObj.devProfiles[3].skills.frameworks;
+        document.getElementById("database").innerHTML = myObj.devProfiles[3].skills.database;
+        document.getElementById("position").innerHTML = myObj.devProfiles[3].skills.position;
+        document.getElementById("description").innerHTML = myObj.devProfiles[3].description;
+        document.getElementById("email").innerHTML = myObj.devProfiles[3].email;
+        document.getElementById("githubProfile").innerHTML = myObj.devProfiles[3].githubProfile;
+
+      }else if(window.location.pathname == '/humphreys-dev-profile.html'){
+       
+        document.getElementById("name").innerHTML = myObj.devProfiles[4].name;
+        document.getElementById("major").innerHTML = myObj.devProfiles[4].major;
+        document.getElementById("concentration").innerHTML = myObj.devProfiles[4].concentration;
+        document.getElementById("degree").innerHTML = myObj.devProfiles[4].degree;
+        document.getElementById("languages").innerHTML = myObj.devProfiles[4].skills.languages;
+        document.getElementById("frameworks").innerHTML = myObj.devProfiles[4].skills.frameworks;
+        document.getElementById("database").innerHTML = myObj.devProfiles[4].skills.database;
+        document.getElementById("position").innerHTML = myObj.devProfiles[4].skills.position;
+        document.getElementById("description").innerHTML = myObj.devProfiles[4].description;
+        document.getElementById("email").innerHTML = myObj.devProfiles[4].email;
+        document.getElementById("githubProfile").innerHTML = myObj.devProfiles[4].githubProfile;
+
+      }else if(window.location.pathname == '/owen-dev-profile.html'){
+       
+        document.getElementById("name").innerHTML = myObj.devProfiles[5].name;
+        document.getElementById("major").innerHTML = myObj.devProfiles[5].major;
+        document.getElementById("concentration").innerHTML = myObj.devProfiles[5].concentration;
+        document.getElementById("degree").innerHTML = myObj.devProfiles[5].degree;
+        document.getElementById("languages").innerHTML = myObj.devProfiles[5].skills.languages;
+        document.getElementById("frameworks").innerHTML = myObj.devProfiles[5].skills.frameworks;
+        document.getElementById("database").innerHTML = myObj.devProfiles[5].skills.database;
+        document.getElementById("position").innerHTML = myObj.devProfiles[5].skills.position;
+        document.getElementById("description").innerHTML = myObj.devProfiles[5].description;
+        document.getElementById("email").innerHTML = myObj.devProfiles[5].email;
+        document.getElementById("githubProfile").innerHTML = myObj.devProfiles[5].githubProfile;
+
+      }else if(window.location.pathname == '/Haruki-dev.html'){
+       
+        document.getElementById("name").innerHTML = myObj.devProfiles[6].name;
+        document.getElementById("major").innerHTML = myObj.devProfiles[6].major;
+        document.getElementById("concentration").innerHTML = myObj.devProfiles[6].concentration;
+        document.getElementById("degree").innerHTML = myObj.devProfiles[6].degree;
+        document.getElementById("languages").innerHTML = myObj.devProfiles[6].skills.languages;
+        document.getElementById("frameworks").innerHTML = myObj.devProfiles[6].skills.frameworks;
+        document.getElementById("database").innerHTML = myObj.devProfiles[6].skills.database;
+        document.getElementById("position").innerHTML = myObj.devProfiles[6].skills.position;
+        document.getElementById("description").innerHTML = myObj.devProfiles[6].description;
+        document.getElementById("email").innerHTML = myObj.devProfiles[6].email;
+        document.getElementById("githubProfile").innerHTML = myObj.devProfiles[6].githubProfile;
+
       }else{
         console.log("this is not the profile page");
       }
